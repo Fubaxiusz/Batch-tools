d6463245148f99f6f76b3cf9d1c26926319971de
Fubaxiusz <jakub.m.fober@pm.me>
2021-07-25 09:43:44 +0200

    Create 7-zip_icons_change.cmd

---
+++ General scripts/7-zip_icons_change.cmd
@@ -0,0 +1,23 @@
+REM This script changes registry values for 7-zip icons
+chcp 65001
+echo off
+cls
+title 7-zip Icon Changer
+echo This script will change icons for 7-zip files
+echo such as .7z, .001, etc...
+echo.
+set /p folder="Drag and drop icons folder> "
+cd /d %folder%
+title 7-zip Icon Changer - Please wait...
+echo Making registry changes. Please wait...
+echo.
+FOR %%e IN (*.ico) DO (
+	echo HKCR\7-Zip.%%~ne\DefaultIcon "%cd%\%%e"
+	REG ADD HKCR\7-Zip.%%~ne\DefaultIcon /ve /d "%cd%\%%e" /f
+	echo.
+)
+title 7-zip Icon Changer - Finished
+echo Finished.
+pause
+endlocal
+goto :eof
